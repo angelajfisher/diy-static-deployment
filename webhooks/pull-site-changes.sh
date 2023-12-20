d7616f9e41c130cf85fd570ac729ae70e8175abe
Angela Fisher <31549337+angelajfisher@users.noreply.github.com>
2023-12-19 20:53:05 -0800

    Webhook complete!

--- webhooks/pull-site-changes.sh
+++ webhooks/pull-site-changes.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
-headers=$(curl -L --include \
+rm -r *
+
+curl -L -o "build.zip" \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: Bearer <YOUR-TOKEN>" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   https://api.github.com/repos/angelajfisher/static-deployment-test/actions/artifacts/$ARTIFACT_ID/zip)
 
-echo $headers
+unzip -o build.zip
+
+rm build.zip
