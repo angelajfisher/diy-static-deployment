86007fe55e10329d5b60e41e12e7a56166c8fc87
Angela Fisher <31549337+angelajfisher@users.noreply.github.com>
2023-12-20 00:41:02 -0800

    Increase script safety

--- webhooks/pull-site-changes.sh
+++ webhooks/pull-site-changes.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
-rm -r *
+set -eu -o pipefail
 
-curl -L -o "build.zip" \
+curl -L -o "/var/www/build.zip" \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: Bearer <YOUR-TOKEN>" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
-  https://api.github.com/repos/angelajfisher/static-deployment-test/actions/artifacts/$ARTIFACT_ID/zip)
+  "https://api.github.com/repos/angelajfisher/static-deployment-test/actions/artifacts/$ARTIFACT_ID/zip"
 
-unzip -o build.zip
+rm -r -- * || true
 
-rm build.zip
+unzip -o "/var/www/build.zip"
+
+rm "/var/www/build.zip"
